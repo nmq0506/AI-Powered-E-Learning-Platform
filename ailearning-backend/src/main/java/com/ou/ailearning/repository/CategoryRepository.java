0977dc858b51019c070f83bedc6315fdc4ee811e
tandzhl <tanbogoodwow@gmail.com>
2026-04-02 17:40:17 +0700

    Fix categories mapper and repository

--- ailearning-backend/src/main/java/com/ou/ailearning/repository/CategoryRepository.java
+++ ailearning-backend/src/main/java/com/ou/ailearning/repository/CategoryRepository.java
@@ -4,10 +4,15 @@ import com.ou.ailearning.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("""
+        SELECT c FROM Category c
+        WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :search, '%'))
+    """)
     Page<Category> findBySearch(@Param("search") String search, Pageable pageable);
 }
--- ailearning-backend/src/main/java/com/ou/ailearning/service/mapper/CategoryMapper.java
+++ ailearning-backend/src/main/java/com/ou/ailearning/service/mapper/CategoryMapper.java
@@ -2,7 +2,9 @@ package com.ou.ailearning.service.mapper;
 
 import com.ou.ailearning.dto.response.CategoryResponse;
 import com.ou.ailearning.entity.Category;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CategoryMapper {
     public CategoryResponse toResponse(Category category) {
         if (category == null) return null;
